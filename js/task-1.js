9123814dd3932f78ddb37839bbb6fd338547bfe3
Liudmyla Kirichenko <liudmyla.deleychuk@gmail.com>
2025-05-09 19:01:26 +0300

    hw7

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const categoriesListElements = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${categoriesListElements.length}`);
+
+categoriesListElements.forEach((category) => {
+  const title = category.querySelectorAll("h2").textContent;
+  const elementsCount = category.querySelectorAll("ul li").length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementsCount}`);
+});
---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const bodyElem = document.body;
+const btnElem = document.querySelector(".change-color");
+const colorSpanElem = document.querySelector(".color");
+
+btnElem.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  bodyElem.style.backgroundColor = randomColor;
+  colorSpanElem.textContent = randomColor;
+});
