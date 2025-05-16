8ed0d55c7250615416dfaa7d1b9959614fe97ebd
Liudmyla Kirichenko <liudmyla.deleychuk@gmail.com>
2025-05-16 18:32:51 +0300

    second commit

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ const categoriesListElements = document.querySelectorAll("#categories .item");
 console.log(`Number of categories: ${categoriesListElements.length}`);
 
 categoriesListElements.forEach((category) => {
-  const title = category.querySelectorAll("h2").textContent;
+  const title = category.querySelector("h2").textContent;
   const elementsCount = category.querySelectorAll("ul li").length;
 
   console.log(`Category: ${title}`);
